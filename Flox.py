88f9f7196d398db7101f15739defa07dbf3d8c6a
Tate Mioton <tatemioton@outlook.com>
2020-05-11 22:16:32 -0500

    Add Main Simulation File
    The goal for this file is to abstract away as much math and OpenGL code as possible.

---
+++ Flox.py
@@ -0,0 +1,42 @@
+import pyglet
+
+from boid import Flock
+from shader import get_default_shader
+from vectors import Vec2
+
+
+class Simulation:
+    FPS = 30.
+
+    def __init__(self, width: int, height: int):
+        self.width = width
+        self.height = height
+
+        self.window = pyglet.window.Window(width, height, "Flox")
+        self.fps = pyglet.window.FPSDisplay(self.window)
+
+        self.shader = get_default_shader()
+        self.shader.set_world_matrix(width, height)
+
+        self.flock = Flock(50, Vec2(0., 0.), min(width, height) / 3)
+
+    def update(self, dt: float):
+        self.flock.update(dt)
+
+    def on_draw(self):
+        self.window.clear()
+
+        with self.shader:
+            self.flock.draw()
+
+        self.fps.draw()
+
+    def run(self):
+        self.window.push_handlers(self.on_draw)
+        pyglet.clock.schedule_interval(self.update, 1. / self.FPS)
+        pyglet.app.run()
+
+
+if __name__ == '__main__':
+    sim = Simulation(800, 600)
+    sim.run()
